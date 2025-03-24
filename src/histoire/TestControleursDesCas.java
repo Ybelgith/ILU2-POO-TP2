1cae716deea7f1a17062f5f9588df7af93ee1df5
Yassine Belghith <ybelghiith@gmail.com>
2025-03-24 08:02:06 +0100

    FIN TP2-3

--- src/histoire/TestControleursDesCas.java
+++ src/histoire/TestControleursDesCas.java
@@ -73,7 +73,7 @@ public class TestControleursDesCas {
 		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(
 				controlVerifierIdentite, village);
 		int numeroEtal = controlPrendreEtal.prendreEtal("Obelix", "fleur", 5);
-		System.out.println("Obelix a pris l'étal n°" + numeroEtal);
+		System.out.println("Obelix a pris l'étal n°" + (numeroEtal+1));
 
 //		Sortie Console :
 //		Cas 4 : Prendre etal
